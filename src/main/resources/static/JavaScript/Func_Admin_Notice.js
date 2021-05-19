0ba8e64bbadeccf9fc2b6119782bd3e3bcc5c727
user <user@61.77.76.167>
2021-05-20 04:10:04 +0900

    20210520_gamza_메뉴바 색감 변경

--- src/main/resources/static/JavaScript/Func_Admin_Notice.js
+++ src/main/resources/static/JavaScript/Func_Admin_Notice.js
@@ -1,7 +1,7 @@
 function selectCurrentMenu() {
 	$(document).ready(function(){
-		$(".menuName").eq(3).css("background-color", "#BDBDBD");
-		$(".menuName").eq(3).children().css("color", "#000000");
+		$(".menuName").eq(3).css("background-color", "#424242");
+		$(".menuName").eq(3).children().css("color", "#FFFFFF");
 		$(".subMenuName").css("visibility", "visible");
 		$(".subMenuName").eq(0).children().css("font-weight", "bold");
 	});
