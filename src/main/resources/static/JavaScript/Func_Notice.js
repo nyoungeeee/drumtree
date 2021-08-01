60bd7df086939c581b83a0c398a91e232c97c744
user <user@210.91.89.159>
2021-08-01 23:22:25 +0900

    20210801_공지사항 열고/닫기 기능 추가, 첨부 파일 없어도 목록 나오는 오류 수정

--- src/main/resources/static/JavaScript/Func_Notice.js
+++ src/main/resources/static/JavaScript/Func_Notice.js
@@ -46,39 +46,51 @@ function processAjax(param0, param1) {
     		$("tbody .noticeList").fadeOut(0);
     		$("tbody .noticeList").fadeIn(500);
     		
-    		if ($("#fileList a").length==0) {
-    			$("#fileList").css("display", "none");
-    		}
+    		
     		
     		$("tbody .noticeList").click(function(){
-    			$(".threadList").css("display", "none");
-    			$(".noticeList").css("background-color", "");
-    			$(".noticeList").css("border", "");
-    			$(".noticeList").css("border-bottom", "1px solid #A4A4A4");
-    			$(".noticeList").children().css("color", "");
-    			
     			var threadNo = "threadNo." + $(this).children().eq(0).html();
-    			$(this).css("background-color", "#585858");
-    			$(this).css("border-top", "2px solid #585858");
-    			$(this).css("border-bottom", "1px solid #585858");
-    			$(this).css("border-left", "1px solid #585858");
-    			$(this).css("border-right", "1px solid #585858");
-    			$(this).children().css("color", "#FFFFFF");
-    			$(".threadList[name='" + threadNo + "']").css("display", "table-row");
-    			$(".threadList[name='" + threadNo + "'] td").fadeOut(0);
-    			$(".threadList[name='" + threadNo + "'] td").fadeIn(250);
     			
-    			$.ajax({
-	    			url: "http://" + IPstring + "/notices",
-	    			data: { noticeIdx: $(this).children().eq(0).html() },
-	    			method: "POST",
-					dataType: "JSON",
-					error: function() { alert("데이터 로드 실패"); },
-					success: function(data) {
-						$(".noticeList[name='" + "noticeNo." + data[0].noticeIdx + "']").children().eq(3).html(data[0].hit);
-						$(".threadList[name='" + "threadNo." + data[0].noticeIdx + "']").find("#noticeHit").html("<strong>조회수:</strong>&nbsp;" + data[0].hit);
+    			if ($(".threadList[name='" + threadNo + "']").css("display")=="table-row") {
+    				$(this).css("background-color", "");
+    				$(this).css("border", "");
+    				$(this).css("border-bottom", "1px solid #A4A4A4");
+    				$(this).children().css("color", "");
+    				$(".threadList[name='" + threadNo + "']").css("display", "none");
+    			}
+    			else {
+    				var fileCount = $(".threadList[name='" + threadNo + "'] #fileList span").length;
+    				if (fileCount==0) {
+						$(".threadList[name='" + threadNo + "'] #fileList").css("display", "none");
 					}
-    			})
+    				
+    				$(".threadList").css("display", "none");
+        			$(".noticeList").css("background-color", "");
+        			$(".noticeList").css("border", "");
+        			$(".noticeList").css("border-bottom", "1px solid #A4A4A4");
+        			$(".noticeList").children().css("color", "");
+        			$(this).css("background-color", "#585858");
+        			$(this).css("border-top", "2px solid #585858");
+        			$(this).css("border-bottom", "1px solid #585858");
+        			$(this).css("border-left", "1px solid #585858");
+        			$(this).css("border-right", "1px solid #585858");
+        			$(this).children().css("color", "#FFFFFF");
+        			$(".threadList[name='" + threadNo + "']").css("display", "table-row");
+        			$(".threadList[name='" + threadNo + "'] td").fadeOut(0);
+        			$(".threadList[name='" + threadNo + "'] td").fadeIn(250);
+        			
+        			$.ajax({
+    	    			url: "http://" + IPstring + "/notices",
+    	    			data: { noticeIdx: $(this).children().eq(0).html() },
+    	    			method: "POST",
+    					dataType: "JSON",
+    					error: function() { alert("데이터 로드 실패"); },
+    					success: function(data) {
+    						$(".noticeList[name='" + "noticeNo." + data[0].noticeIdx + "']").children().eq(3).html(data[0].hit);
+    						$(".threadList[name='" + "threadNo." + data[0].noticeIdx + "']").find("#noticeHit").html("<strong>조회수:</strong>&nbsp;" + data[0].hit);
+    					}
+        			})
+    			}
     		})
         }
     })
